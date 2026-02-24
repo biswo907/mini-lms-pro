085a45d3b5b1f0bf54de4cb6b3ff44b1fd5c8192
biswo907 <biswajitdash907@gmail.com>
2026-02-24 19:24:25 +0530

    Remove Some Unnecessary Files

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,12 @@
-import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 
-export default function ModalScreen() {
+export default function HomeScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
+      <ThemedText type="title">This is a Home Screen</ThemedText>
     </ThemedView>
   );
 }
@@ -21,9 +17,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+  }
 });
